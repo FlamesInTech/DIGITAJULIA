8eebf8679ea6f432f62a9a865085641f7315a27d
Shalom <adoyishalom@gmail.com>
2022-05-25 14:07:32 +0100

    first commit for digitajulia website

---
+++ script.js
@@ -0,0 +1,50 @@
+    $(".carousel").owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 1,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+        }
+    });
+
+let hamburger = document.querySelector(".hamburger");
+let navLinks = document.querySelector(".nav-links");
+let links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Animation
+    hamburger.classList.toggle("toggle");
+});
+
+navLinks.addEventListener('click', ()=>{
+   //Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Animation
+    hamburger.classList.toggle("toggle");
+});
+
+
+
